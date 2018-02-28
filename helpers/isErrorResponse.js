1df525823e7ef4729b8dedffbb06848265e9a871
Rafał Lewandowski <r.lewandowski@kainos.com>
2018-02-28 16:08:17 +0000

    Provide additional isErrorResponse tests

---
+++ helpers/isErrorResponse.js
@@ -0,0 +1,11 @@
+/**
+ * Checks if the returned response is an error response.
+ *
+ * @param response a Node's http.IncomingMessage object
+ * @return boolean true if the response status is 4XX or 5XX
+ */
+function isErrorResponse(response) {
+  return 400 <= response.statusCode && response.statusCode < 600
+}
+
+module.exports = isErrorResponse
--- index.js
+++ index.js
@@ -11,6 +11,7 @@ var extend = require('extend');
 var when = require('when');
 var request = require('request');
 var RetryStrategies = require('./strategies');
+var isErrorResponse = require('./helpers/isErrorResponse')
 var _ = require('lodash');
 
 var DEFAULTS = {
@@ -42,16 +43,6 @@ function makePromise(requestInstance, promiseFactoryFn) {
   }
 
   return promiseFactoryFn(Resolver.bind(requestInstance));
-}
-
-/**
- * Checks if the returned response is an error response.
- *
- * @param response a Node's http.IncomingMessage object
- * @return boolean true if the response status is 4XX or 5XX
- */
-function isErrorResponse(response) {
-  return 400 <= response.statusCode && response.statusCode < 600
 }
 
 function Request(url, options, f, retryConfig) {
