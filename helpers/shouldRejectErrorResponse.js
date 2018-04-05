dd43fac4fb6d5e90c20859a9177c8d41576dda6b
Sabah Irfan <sabah.irfan@gmail.com>
2018-04-05 14:00:40 +0100

    Code review comments

--- helpers/shouldRejectErrorResponse.js
+++ helpers/shouldRejectErrorResponse.js
@@ -1,14 +1,15 @@
 /**
  * Checks if the options.simple is set to use default.
+ * By default, http response codes other than 2xx will cause the promise to be rejected. This can be overwritten by setting `options.simple = false`.
  *
  * @param options a request.options object
- * @return boolean true if options.simple is set to use default value
+ * @return boolean true if non successful response should be rejected, otherwise false
  */
 function shouldRejectErrorResponse (options) {
   if (!options) {
     return true;
   }
-  return (options.simple === undefined || options.simple === true);
+  return options.simple === undefined || options.simple === true;
 }
 
 module.exports = shouldRejectErrorResponse;
--- test/helpers/shouldRejectErrorResponse.test.js
+++ test/helpers/shouldRejectErrorResponse.test.js
@@ -4,6 +4,15 @@ const shouldRejectErrorResponse = require('../../helpers/shouldRejectErrorRespon
 const expect = require('chai').expect;
 
 describe('shouldRejectErrorResponse', function () {
+
+  it('should return true for options is null', function () {
+    expect(shouldRejectErrorResponse(null)).to.equal(true);
+  });
+
+  it('should return true for options is undefined', function () {
+    expect(shouldRejectErrorResponse(undefined)).to.equal(true);
+  });
+
   [{ simple: true }, {}].forEach(options => {
     it(`should return true for ${options.simple} simple`, function () {
       expect(shouldRejectErrorResponse({ options: options })).to.equal(true);
@@ -12,14 +21,6 @@ describe('shouldRejectErrorResponse', function () {
 
   it('should return false for options.simple false', function () {
     expect(shouldRejectErrorResponse({ simple: false })).to.equal(false);
-  });
-
-  it('should return true for options is null', function () {
-    expect(shouldRejectErrorResponse(null)).to.equal(true);
-  });
-
-  it('should return true for options is undefined', function () {
-    expect(shouldRejectErrorResponse(undefined)).to.equal(true);
   });
 
 });
